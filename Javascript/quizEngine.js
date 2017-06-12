b3a7b267115e8702233d23353d035a611a4a8c25
Gyhth <Gyhthy@gmail.com>
2017-06-12 09:09:30 -0400

    Splitting out Scripts
    Engine how has its own Javascript file, the user can add questions in a seperate file without needing to worry about the engine file at all.

--- Javascript/quizEngine.js
+++ Javascript/quizEngine.js
@@ -12,32 +12,15 @@ function answer(answerText, correct) {
   this.correct = correct;
 }
 
-questions = new Array();
-answersOne = new Array();
-answersOne.push(new answer("Yes", true));
-answersOne.push(new answer("No", false));
-questions.push(new question(false, "The application is working?", answersOne, "Yes, the application is working.", null, false));
-
-answersTwo = new Array();
-answersTwo.push(new answer("Checkbox One", true));
-answersTwo.push(new answer("Checkbox Two", true));
-answersTwo.push(new answer("Checkbox Three", true));
-questions.push(new question(true, "Example of Checkboxes.", answersTwo, "To get this correct, you needed to select all the values.", null, false));
-
-$question = $("#question");
-$content = $("#content");
-
-$(function() {
-  var quizEngine = new quizEngineClass(true, 500, questions);
-});
-
 var quizEngineClass = function(displayKeyboard, animationSpeed, questions) {
   this.animationSpeed = animationSpeed;
   this.displayKeyboard = displayKeyboard;
   this.questions = questions;
-  $("body").append('<div id="PopUp"></div>');
-  $Popup = $("#PopUp");
-  $Popup.hide();
+  if (!$("#Popup").length) {
+    $("body").append('<div id="PopUp"></div>');
+    $Popup = $("#PopUp");
+    $Popup.hide();
+  }
   this.displayIntro();
 };
 
